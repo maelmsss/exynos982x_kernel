84f8adcb233c05b98ef63b505569ffa982c1e1f6
maelmsss <handsofgodd2@gmail.com>
2026-09-11 01:33:04 +0300

    Update fair.c
    Signed-off-by: maelmsss <handsofgodd2@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -8575,24 +8575,12 @@ static void yield_task_fair(struct rq *rq)
 	/*
 	 * Are we the only task in the tree?
 	 */
+	#if !defined(CONFIG_SCHED_BORE)
 	if (unlikely(rq->nr_running == 1))
 		return;
 
 	clear_buddies(cfs_rq, se);
-
-	if (curr->policy != SCHED_BATCH) {
-		update_rq_clock(rq);
-		/*
-		 * Update run-time statistics of the 'current'.
-		 */
-		update_curr(cfs_rq);
-		/*
-		 * Tell update_rq_clock() that we've just updated,
-		 * so we don't do microscopic update in schedule()
-		 * and double the fastpath cost.
-		 */
-		rq_clock_skip_update(rq, true);
-	}
+	#endif // CONFIG_SCHED_BORE
 
 	set_skip_buddy(se);
 }
