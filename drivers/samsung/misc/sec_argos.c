0efdf72b49ab7248620f915c21c8ff325d05968f
maelmsss <handsofgodd2@gmail.com>
2026-09-15 19:39:22 +0300

    Update sec_argos.c
    Signed-off-by: maelmsss <handsofgodd2@gmail.com>

--- drivers/samsung/misc/sec_argos.c
+++ drivers/samsung/misc/sec_argos.c
@@ -371,10 +371,7 @@ int argos_irq_affinity_apply(int dev_num, bool enable)
 			mask = this->default_cpu_mask;
 		}
 
-		if (IS_ENABLED(CONFIG_IRQ_SBALANCE))
-			result = 0;
-		else
-			result = irq_set_affinity_hint(this->irq, mask);
+		result = irq_set_affinity(this->irq, mask);
 
 
 		pr_info("%s: irq%d affinity %s to cpu_mask:0x%X\n",
